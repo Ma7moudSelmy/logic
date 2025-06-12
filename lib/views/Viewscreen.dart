f2809d5388eba9e64ed540f977db6d8915a3860f
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-06-12 16:46:12 +0300

    Refactor main application structure and add initial view screens

---
+++ lib/views/Viewscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Viewscreen extends StatefulWidget {
+  const Viewscreen({super.key});
+
+  @override
+  State<Viewscreen> createState() => _ViewscreenState();
+}
+
+class _ViewscreenState extends State<Viewscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/views/mulit_image_selection.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MulitImageSelection extends StatelessWidget {
+  const MulitImageSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+            backgroundColor: Colors.blue
+
+    );
+  }
+}
